f4c2ad3691b8c44699953a5b4ea1bb37bf452fcc
Josh Dingus <josh.dingus@gmail.com>
2014-08-01 16:01:09 -0400

    split time enty by given minute interval complete

--- time_split.py
+++ time_split.py
@@ -1,69 +1,78 @@
 from datetime import timedelta, date, time, datetime
+from dateutil.rrule import *
 
 def main():
-	start = datetime(2009, 6, 14, 21, 0, 0)
-	stop = datetime(2009, 6, 14, 23, 0, 0) 
-	time_split_hour(start,stop)
+    start = datetime(2009, 6, 14, 4, 0, 0)
+    stop = datetime(2009, 6, 14, 4, 1, 10) 
 
+    time_list=time_split(start,stop,1)
+    print time_list
 
-def time_split_hour(start,stop):
-	""" Using a start and stop time return a list of allotted time by hour """
+def round_down_minute(time_,minute_interval):
+    """ Pass in datetime and round down to nearest minute interval """
+    time_ = time_ - timedelta(minutes=time_.minute % minute_interval,
+                     seconds=time_.second,
+                     microseconds=time_.microsecond)
+    return time_
 
-	TH_bg = start.replace(minute = 0, second = 0, microsecond = 0)
-	# TH_end = stop
-	# TH_end -= timedelta(minutes = stop.minute, seconds = stop.second, microseconds =  stop.microsecond)
+def time_by_minute(start,stop,minute_interval):
+    """ start and stop time passed in and return list of times in 
+    period encompasing start and stop times at given minute interval """
+    tpb = round_down_minute(start,minute_interval)
+    tpe = round_down_minute(stop,minute_interval) + \
+        timedelta(minutes=minute_interval)
+    
+    minute_list = list(rrule(MINUTELY, interval=minute_interval, \
+        dtstart = tpb, until = tpe))
+    return minute_list
 
-	TH_end = stop - timedelta(minutes = stop.minute, seconds = stop.second, microseconds =  stop.microsecond)
-	print 'TH_bg=',TH_bg
-	print 'TH_end=',TH_end
-	pass
+def time_split(start,stop,minute_interval):
+    """ Using a start and stop time return a list of allotted time by hour """
+    minute_list = time_by_minute(start,stop,minute_interval)
+        
+    time_list = []
 
-def split_by_hour(start,stop):
-	""" Using a START and STOP time return a list of hours with all of the time
-	bucketed by hour """
-	hour_list = return_hour_list(start,stop) # creates interval of every 1 hour based on start/stop
-	ListEvents = [] # list used to store all the events which are by hour
-	print "Hour List",hour_list
+    i=0
+    for item in minute_list:
+        i=i+1
+        tint_bg = item
+        tint_end = tint_bg + timedelta(minutes=minute_interval)
+        """ First loop different than rest """
+        temp_dict={}
 
-	for item in hour_list:
-		temp_dict = {}
-		# ev_bucket = item.strftime('%m/%d/%y %H:%M:%S')  #Format '06/14/09 19:00:00'
-		TP_bg = item 
-		TP_end = item + timedelta(hours=1)
-		TH_bg = hour_list[0]
-		TH_end = hour_list[-1]
-		
-		if TH_bg == TH_end:
-			print "This is a completely closed case, must be single entry within same hour!"
-			ev_dur = convert_timedelta(stop - start) # returns value in seconds
-			temp_dict[TP_bg] = ev_dur # Store this single entry into dict, ONLY one so we are DONE
-			ListEvents.append(temp_dict)
-			return ListEvents
+        if stop < tint_bg:
+            continue
+            # return time_list
 
-		if TH_bg == TH_end:
-			print "This is a completely closed case, must be single entry within same hour!"
-			ev_dur = convert_timedelta(stop - start) # returns value in seconds
-			temp_dict[TP_bg] = ev_dur # Store this single entry into dict, ONLY one so we are DONE
-			ListEvents.append(temp_dict)
-			return ListEvents
+        if i==1: 
+            if stop <= tint_end:    #A            # In this case only one hour
+                duration = stop - start
+                temp_dict[tint_bg] = duration
+                time_list.append(temp_dict)
 
-def return_hour_list(start,stop):
-	""" Returns a list of hours to check for when given a start and stop time """
-			
-	""" return INT_HB and INT_HE base on start and stop time  """
-	INT_HB = start.replace(minute = 0, second = 0, microsecond = 0)
-	# INT_HB -= timedelta(minutes = start.minute, seconds	= start.second, microseconds = start.microsecond)
-	# print "INT_HB",INT_HB
+            if stop > tint_end:     #B
+                duration = tint_end - start
+                temp_dict[(tint_bg)] = duration
+                time_list.append(temp_dict)
 
-	INT_HE = stop
-	INT_HE -= timedelta(minutes = stop.minute, seconds = stop.second, microseconds =  stop.microsecond)
-	# print "INT_HE",INT_HE
+        else:
+            if stop <= tint_end:     #C
+                duration = stop - tint_bg
+                temp_dict[(tint_bg)] = duration
+                time_list.append(temp_dict)
 
-	""" Create a list of hours to check for time """
-	hour_list = list(rrule(HOURLY, interval=1, dtstart = INT_HB, until = INT_HE))
-	
-	# print hour_list
-	return hour_list
+            if stop > tint_end:      #D
+                duration = tint_end - tint_bg
+                temp_dict[tint_bg] = duration
+                time_list.append(temp_dict)
+
+    """ Remove 0 timedelta entries and empty dicts """
+    for dict in time_list:
+        for k, v in dict.items():
+            if v.total_seconds() == 0:
+                dict.pop(k)
+    time_list = [x for x in time_list if x]
+    return time_list
 
 if __name__ == "__main__":   
-	main()
+    main()
